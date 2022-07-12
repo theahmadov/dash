17e30ad83641465612a890b868346449b4fe04b3
Error <88781785+TheSadError@users.noreply.github.com>
2022-07-12 18:32:01 +0400

    Create githubscrape.py

---
+++ scanlib/githubscrape.py
@@ -0,0 +1,20 @@
+import json
+from re import I
+import requests
+from colorama import *
+import js
+
+def getinfo(username):
+    url = "https://api.github.com/users/{}".format(username)
+    data = requests.get("https://api.github.com/users/{}".format(username)).text
+    js.jsd("error", data)
+    need = [
+            'name',
+            'type',
+            'company',
+            'location',
+            'email'
+        ]
+    req = requests.get(url).json()
+    dt = js.pdata(need, req)
+    print(json.dumps(dt, indent= True))

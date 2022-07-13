e72a2f14aef856febed52143960931ced4fad6f0
Error <88781785+TheSadError@users.noreply.github.com>
2022-07-13 12:15:17 +0400

    Create sites.py

---
+++ sites.py
@@ -0,0 +1,24 @@
+# Social Media Websites : 
+udata = [
+        "https://github.com/{}", # 1
+        "https://instagram.com/{}",# 2
+        "https://twitter.com/{}",# 3
+        "https://t.me/{}", # 4
+        "https://{}.tumblr.com/",# 5
+        "https://tieba.baidu.com/f?kw={}",# 6
+        "https://www.pinterest.com/{}",# 7
+        "https://vk.com/{}", # 8
+        "https://www.reddit.com/user/{}", # 9
+        "https://mix.com/{}", # 10
+        "https://{}.skyrock.com/", # 11
+        "https://www.facebook.com/{}", # 12
+        "https://bionluk.com/{}", # 13
+        "https://gitlab.com/{}", # 14
+        "https://www.tiktok.com/@{}", # 15
+        "https://www.quora.com/{}", # 16
+        "https://medium.com/@{}", # 17
+        "https://digg.com/@{}", # 18
+        "https://www.linkedin.com/in/{}", # 19
+        "https://www.deviantart.com/{}", # 20
+        "https://www.twitch.tv/{}" # 21	
+]
